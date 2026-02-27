fd8f0da9935f8e590f2e987ec6260b48af814730
23f2001831 <kirandwip.info@gmail.com>
2026-02-27 11:12:07 +0530

    Fix Render deployment: bind gunicorn to PORT and remove Vercel configs

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn image_converter_flask:app
+web: gunicorn image_converter_flask:app --bind 0.0.0.0:$PORT
--- api/index.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-# Add the parent directory to the path so we can import the Flask app
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from image_converter_flask import app
-
-# This 'app' variable will be automatically detected by Vercel as a WSGI application
