78ae7c85a2418cdda2de77fb0fce9180feecba1b
23f2001831 <kirandwip.info@gmail.com>
2026-02-27 10:56:58 +0530

    Simplify Vercel config: remove functions property for auto-detection

--- api/index.py
+++ api/index.py
@@ -1,5 +1,3 @@
-# api/index.py
-# Vercel serverless function handler for Flask app
 import sys
 import os
 
@@ -8,5 +6,4 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from image_converter_flask import app
 
-# Export the Flask app as 'app' for Vercel's WSGI handler
-# Vercel will automatically detect this and serve it
+# This 'app' variable will be automatically detected by Vercel as a WSGI application
